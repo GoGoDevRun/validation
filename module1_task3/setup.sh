1153ed5ded3f74306d116da49fe2a58afa4b1386
Rodrigo Medina <rodrigomedina@recursoconfiable.com>
2021-08-26 11:48:34 -0500

    Add site

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ##docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 ./start.sh
-apt-get update && apt-get install -y make
+apt-get update && apt-get install -y make hugo
 make build
 exit
