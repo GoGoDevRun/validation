e85b1d80875ffbfee36225dfed9302fe03554851
Rodrigo Medina <rodrigomedina@recursoconfiable.com>
2021-08-26 08:18:30 -0500

    docker without name

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app --name GoGoDevRun ubuntu:18.04 ./start.sh
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 ./start.sh
